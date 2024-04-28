190605c55301531cc94d818721156e294451b8ae
PatrickEdwards485 <ppedward@syr.edu>
2024-04-28 15:45:00 -0400

    Correct Vote Totals.
    Now tabulate only 'DISTRICT VOTES:' correlated data.

--- congressional_elections.py
+++ congressional_elections.py
@@ -314,3 +314,178 @@ relevant_districts_2010 = df_2010[(df_2010['DISTRICT'].isin(['10', '21', '25']))
 print(relevant_districts_2010)
 
 print(relevant_districts_2010['DISTRICT'].unique())
+
+
+#%%
+# PREVIOUS congressional_elections_2012_census.py SCRIPT BEFORE CREATING UNIVERSAL Y-AXIS SCALE
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Read the 2012 Excel file
+file_path_2012 = "2012congresults.xls"
+df_2012 = pd.read_excel(file_path_2012, sheet_name='2012 US House & Senate Results')
+
+# Read the 2014 Excel file
+file_path_2014 = "results14.xls"
+df_2014 = pd.read_excel(file_path_2014, sheet_name='2014 US House Results by State')
+
+# Read the 2016 Excel file
+file_path_2016 = "federalelections2016.xlsx"
+df_2016 = pd.read_excel(file_path_2016, sheet_name='2016 US House Results by State')
+
+# Read the 2018 Excel file
+file_path_2018 = "federalelections2018.xlsx"
+df_2018 = pd.read_excel(file_path_2018, sheet_name='2018 US House Results by State')
+
+# Read the 2020 Excel file
+file_path_2020 = "federalelections2020.xlsx"
+df_2020 = pd.read_excel(file_path_2020, sheet_name='13. US House Results by State')
+
+# Create a dictionary to store the "District Votes:" totals for each district in each year
+district_votes = {
+    '10': {'2012': 237187, '2014': 257725, '2016': 289194, '2018': 312626, '2020': 323937},
+    '17': {'2012': 228328, '2014': 228324, '2016': 257480, '2018': 287600, '2020': 293947},
+    '21': {'2012': 264518, '2014': 278590, '2016': 313702, '2018': 343727, '2020': 361356},
+    '25': {'2012': 240629, '2014': 263649, '2016': 293328, '2018': 320164, '2020': 330193},
+    '31': {'2012': 237187, '2014': 257725, '2016': 289194, '2018': 312626, '2020': 323937}
+}
+
+# Plot bar graphs for each district
+for district in ['10', '17', '21', '25', '31']:
+    plt.figure(figsize=(8, 6))
+    
+    # Calculate maximum vote totals for the current district
+    max_votes_2012 = district_votes[district]['2012']
+    max_votes_2014 = district_votes[district]['2014']
+    max_votes_2016 = district_votes[district]['2016']
+    max_votes_2018 = district_votes[district]['2018']
+    max_votes_2020 = district_votes[district]['2020']
+    
+    # Plot for 2012
+    plt.bar('2012', max_votes_2012, color='red', label='2012')
+    
+    # Plot for 2014
+    plt.bar('2014', max_votes_2014, color='red', label='2014')
+    
+    # Plot for 2016
+    plt.bar('2016', max_votes_2016, color='red', label='2016')
+    
+    # Plot for 2018
+    plt.bar('2018', max_votes_2018, color='red', label='2018')
+    
+    # Plot for 2020
+    plt.bar('2020', max_votes_2020, color='red', label='2020')
+    
+    plt.xlabel('Year')
+    plt.ylabel('Vote Totals')
+    plt.title(f'Vote Totals for District {district} (2012-2020 Elections)')
+    plt.legend()
+    plt.ylim(0, max(max_votes_2012, max_votes_2014, max_votes_2016, max_votes_2018, max_votes_2020) * 1.1)
+    plt.show()
+
+
+
+#%%
+# PREVIOUS congressional_elections_2012_census.py SCRIPT BEFORE ABOVE CHANGE (USING ONLY 'DISTRICT VOTES:' VOTE TOTALS)
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Read the 2012 Excel file
+file_path_2012 = "2012congresults.xls"
+df_2012 = pd.read_excel(file_path_2012, sheet_name='2012 US House & Senate Results')
+
+# Filter relevant districts for 2012 in Texas
+relevant_districts_2012 = df_2012[(df_2012['D'].isin(['10', '17', '21', '25', '31'])) & (df_2012['STATE'] == 'Texas')]
+
+# Clean data for 2012
+relevant_districts_2012.rename(columns={'GENERAL VOTES ': 'GENERAL VOTES'}, inplace=True)
+relevant_districts_2012.loc[:, 'GENERAL VOTES'] = relevant_districts_2012['GENERAL VOTES'].replace('UNOPPOSED', np.nan)
+relevant_districts_2012.loc[:, 'GENERAL VOTES'] = pd.to_numeric(relevant_districts_2012['GENERAL VOTES'], errors='coerce')
+
+# Read the 2014 Excel file
+file_path_2014 = "results14.xls"
+df_2014 = pd.read_excel(file_path_2014, sheet_name='2014 US House Results by State')
+
+# Filter relevant districts for 2014 in Texas
+relevant_districts_2014 = df_2014[(df_2014['D'].isin(['10', '17', '21', '25', '31'])) & (df_2014['STATE'] == 'Texas')]
+
+# Clean data for 2014
+relevant_districts_2014.rename(columns={'GENERAL VOTES ': 'GENERAL VOTES'}, inplace=True)
+relevant_districts_2014.loc[:, 'GENERAL VOTES'] = relevant_districts_2014['GENERAL VOTES'].replace('UNOPPOSED', np.nan)
+relevant_districts_2014.loc[:, 'GENERAL VOTES'] = pd.to_numeric(relevant_districts_2014['GENERAL VOTES'], errors='coerce')
+
+# Read the 2016 Excel file
+file_path_2016 = "federalelections2016.xlsx"
+df_2016 = pd.read_excel(file_path_2016, sheet_name='2016 US House Results by State')
+
+# Filter relevant districts for 2016 in Texas
+relevant_districts_2016 = df_2016[(df_2016['D'].isin(['10', '17', '21', '25', '31'])) & (df_2016['STATE'] == 'Texas')]
+
+# Clean data for 2016
+relevant_districts_2016.rename(columns={'GENERAL VOTES ': 'GENERAL VOTES'}, inplace=True)
+relevant_districts_2016.loc[:, 'GENERAL VOTES'] = relevant_districts_2016['GENERAL VOTES'].replace('UNOPPOSED', np.nan)
+relevant_districts_2016.loc[:, 'GENERAL VOTES'] = pd.to_numeric(relevant_districts_2016['GENERAL VOTES'], errors='coerce')
+
+# Read the 2018 Excel file
+file_path_2018 = "federalelections2018.xlsx"
+df_2018 = pd.read_excel(file_path_2018, sheet_name='2018 US House Results by State')
+df_2018['DISTRICT'] = df_2018['DISTRICT'].astype(str)
+
+# Filter relevant districts for 2018 in Texas
+relevant_districts_2018 = df_2018[(df_2018['DISTRICT'].isin(['10', '17', '21', '25', '31'])) & (df_2018['STATE'] == 'Texas')]
+
+# Clean data for 2018
+relevant_districts_2018.rename(columns={'GENERAL VOTES ': 'GENERAL VOTES'}, inplace=True)
+relevant_districts_2018.loc[:, 'GENERAL VOTES'] = relevant_districts_2018['GENERAL VOTES'].replace('UNOPPOSED', np.nan)
+relevant_districts_2018.loc[:, 'GENERAL VOTES'] = pd.to_numeric(relevant_districts_2018['GENERAL VOTES'], errors='coerce')
+
+# Read the 2020 Excel file
+file_path_2020 = "federalelections2020.xlsx"
+df_2020 = pd.read_excel(file_path_2020, sheet_name='13. US House Results by State')
+
+# Filter relevant districts for 2020 in Texas
+relevant_districts_2020 = df_2020[(df_2020['DISTRICT'].isin(['10', '17', '21', '25', '31'])) & (df_2020['STATE'] == 'Texas')]
+
+# Clean data for 2020
+relevant_districts_2020.rename(columns={'GENERAL VOTES ': 'GENERAL VOTES'}, inplace=True)
+relevant_districts_2020.loc[:, 'GENERAL VOTES'] = relevant_districts_2020['GENERAL VOTES'].replace('UNOPPOSED', np.nan)
+relevant_districts_2020.loc[:, 'GENERAL VOTES'] = pd.to_numeric(relevant_districts_2020['GENERAL VOTES'], errors='coerce')
+
+# Plot bar graphs for each district
+for district in ['10', '17', '21', '25', '31']:
+    plt.figure(figsize=(8, 6))
+    
+    # Calculate maximum vote totals
+    max_votes_2012 = relevant_districts_2012[relevant_districts_2012['D'] == district]['GENERAL VOTES'].sum()
+    max_votes_2014 = relevant_districts_2014[relevant_districts_2014['D'] == district]['GENERAL VOTES'].sum()
+    max_votes_2016 = relevant_districts_2016[relevant_districts_2016['D'] == district]['GENERAL VOTES'].sum()
+    max_votes_2018 = relevant_districts_2018[relevant_districts_2018['DISTRICT'] == district]['GENERAL VOTES'].sum()
+    max_votes_2020 = relevant_districts_2018[relevant_districts_2018['DISTRICT'] == district]['GENERAL VOTES'].sum()
+    
+    # Find the maximum of the two maximum values
+    max_votes = max(max_votes_2012, max_votes_2014, max_votes_2016, max_votes_2018, max_votes_2020)
+    
+    # Plot for 2012
+    plt.bar('2012', relevant_districts_2012[relevant_districts_2012['D'] == district]['GENERAL VOTES'].sum(), color='red', label='2012')
+    
+    # Plot for 2014
+    plt.bar('2014', relevant_districts_2014[relevant_districts_2014['D'] == district]['GENERAL VOTES'].sum(), color='red', label='2014')
+    
+    # Plot for 2016
+    plt.bar('2016', relevant_districts_2016[relevant_districts_2016['D'] == district]['GENERAL VOTES'].sum(), color='red', label='2016')
+    
+    # Plot for 2018
+    plt.bar('2018', relevant_districts_2018[relevant_districts_2018['DISTRICT'] == district]['GENERAL VOTES'].sum(), color='red', label='2018')
+    
+    # Plot for 2020
+    plt.bar('2020', relevant_districts_2020[relevant_districts_2020['DISTRICT'] == district]['GENERAL VOTES'].sum(), color='red', label='2020')
+    
+    plt.xlabel('Year')
+    plt.ylabel('Vote Totals')
+    plt.title(f'Vote Totals for District {district} (2012-2020 Elections)')
+    plt.legend()
+    plt.ylim(0, max_votes * 1.1)
+    plt.show()
+    
